96327dbba735dcc3b4d6048db135210bcaa50bf5
StevenRandallVolbrecht <svolbrecht@yahoo.com>
2018-09-27 11:38:25 -0500

    initial commit with superhero table properties set

---
+++ SuperHero/Models/SuperHeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class SuperHeros
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Alterego { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string Catchphrase { get; set; }
+    }
+}
