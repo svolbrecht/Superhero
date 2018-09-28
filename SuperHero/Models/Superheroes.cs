2ddd18434c39d71f576684288f6a567b56fb7d7a
StevenRandallVolbrecht <svolbrecht@yahoo.com>
2018-09-28 11:50:59 -0500

    no final commits...added display to table names

--- SuperHero/Models/Superheroes.cs
+++ SuperHero/Models/Superheroes.cs
@@ -13,10 +13,13 @@ namespace SuperHero.Models
 
         public string Name { get; set; }
 
+        [Display(Name="Alter Ego")]
         public string Alterego { get; set; }
 
+        [Display(Name="Primary Ability")]
         public string PrimaryAbility { get; set; }
 
+        [Display(Name="Secondary Ability")]
         public string SecondaryAbility { get; set; }
 
         public string Catchphrase { get; set; }
